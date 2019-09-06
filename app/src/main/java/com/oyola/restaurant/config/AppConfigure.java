abf35ec9c381d9eed68d2d0d592861eae9953fb3
Sridhar S <sridhar@appoets.com>
2019-09-06 16:08:02 +0530

    # Setup a new package name and configure the app credentials to use Google and Facebook API services # Remove the unnecessary files from the Git repository

---
+++ app/src/main/java/com/oyola/restaurant/config/AppConfigure.java
@@ -0,0 +1,12 @@
+package com.oyola.restaurant.config;
+
+/**
+ * Created by Tamil on 12/27/2017.
+ */
+
+public class AppConfigure {
+
+    public static String BASE_URL = "https://oyola.com.au/";
+    public static String CLIENT_SECRET = "aCaCS0Kf2MArewwGDgHqfTz9q4U3GRqZwgK1LR70";
+    public static String CLIENT_ID = "2";
+}
---
+++ app/src/main/java/com/oyola/restaurant/controller/ProfileListener.java
@@ -0,0 +1,14 @@
+package com.oyola.restaurant.controller;
+
+import com.oyola.restaurant.model.Profile;
+
+/**
+ * Created by Tamil on 3/16/2018.
+ */
+
+public interface ProfileListener {
+
+    void onSuccess(Profile profile);
+
+    void onFailure(String error);
+}
