227a0b71bf0b07ea951c456e8ba250231feca382
Gopinath <gopinath.m@appoets.com>
2020-10-12 21:09:19 +0530

    Incoming request alert tone issue resolved

--- app/src/main/java/com/dietmanager/restaurant/notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/dietmanager/restaurant/notification/MyFirebaseMessagingService.java
@@ -4,16 +4,22 @@ package com.dietmanager.restaurant.notification;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+import android.util.Log;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
-import android.util.Log;
 
 import com.dietmanager.restaurant.R;
 import com.dietmanager.restaurant.activity.SplashActivity;
+import com.dietmanager.restaurant.application.MyApplication;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -21,6 +27,7 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
+    public static Ringtone mRingtone;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -36,6 +43,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String messageBody) {
+
+        if (messageBody != null && messageBody.equalsIgnoreCase("New Order Received")) {
+            playNotificationSound();
+        }
+
         Log.d(TAG, "messageBody " + messageBody);
         Intent intent = new Intent(getApplicationContext(), SplashActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -80,6 +92,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         }
 
         notificationManager.notify(0, notification);
+
+    }
+
+    public void playNotificationSound() {
+        try {
+            Uri alarmSound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + MyApplication.getInstance().getApplicationContext().getPackageName() + "/raw/alert_tone");
+            mRingtone = RingtoneManager.getRingtone(MyApplication.getInstance().getApplicationContext(), alarmSound);
+            mRingtone.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /*private int getNotificationIcon(NotificationCompat.Builder notificationBuilder) {
